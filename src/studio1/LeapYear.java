7adff1b83e9a0b2e59d902043be206d22904e241
yunfeizhang <yunfeizhang@172.20.8.156>
2022-01-27 12:31:37 -0600

    new leap year

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,15 @@
+package studio1;
+
+import support.cse131.ArgsProcessor;
+
+public class LeapYear {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArgsProcessor ap = new ArgsProcessor(args);
+		int year = ap.nextInt("Input an integer");
+		boolean leap = (year%4 ==0) && (year%100 !=0 || year%400 ==0);
+		System.out.println(year +" is a leap year: " + leap);
+	} 
+
+}
